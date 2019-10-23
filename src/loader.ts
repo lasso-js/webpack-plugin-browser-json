8468ae94f7dc7e2422119d3eda8faf6a95e8b908
Dylan Piercey <dpiercey@ebay.com>
2019-10-23 11:57:31 -0700

    Revert "refactor: load folder browser jsons via post loader"
    This reverts commit 7cb0f9e9cc386e35f0bbd68e082c47c419065047.

--- src/loader.ts
+++ src/loader.ts
@@ -1,38 +1,11 @@
-import path from "path";
 import { loader } from "webpack";
 import { getOptions, stringifyRequest } from "loader-utils";
 import normalizeBrowserJson from "./normalize-browser-json";
-const LOADED_LOOKUP = new WeakMap();
 
-export default function(this: loader.LoaderContext, content: string) {
-  const { _module, _compilation } = this;
+export default async function(this: loader.LoaderContext, content: string) {
   const options = getOptions(this);
-  const loaded: Set<string> =
-    LOADED_LOOKUP.get(_compilation) ||
-    LOADED_LOOKUP.set(_compilation, new Set()).get(_compilation);
   this.cacheable();
 
-  if (_module.type !== "javascript/auto") {
-    return content;
-  }
-
-  if (!options) {
-    const resourceDirBrowserJSONFile = path.join(this.context, "browser.json");
-    if (loaded.has(resourceDirBrowserJSONFile)) {
-      return content;
-    }
-
-    const done = this.async();
-    return this.fs.stat(resourceDirBrowserJSONFile, (err, stat) => {
-      if (err || !stat.isFile()) {
-        return done(null, content);
-      }
-
-      done(null, `require("./browser.json");${content}`);
-    });
-  }
-
-  loaded.add(this.resource);
   return normalizeBrowserJson({
     flags: options.flags,
     content
